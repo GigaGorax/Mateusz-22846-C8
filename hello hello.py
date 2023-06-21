3fd4aa023e1dca558fcdb410fb6869dc22f8103b
GigaGorax <mateuszklysiaksm@gmail.com>
2023-06-21 21:03:42 +0200

    lalalal

--- hello hello.py
+++ hello hello.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("kolejna zmiana")
 =======
 print("helo helo")
 >>>>>>> parent of b9ea3a0 (cos robie i nawet nie licze ile to juz razy)
+=======
+print("zmiana")
+>>>>>>> parent of 64d4445 (kolejna zmiana)
--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("zmiana ze hej hej hej")
 =======
 print("helo")
 >>>>>>> parent of b9ea3a0 (cos robie i nawet nie licze ile to juz razy)
+=======
+print("zmiana ze hej")
+>>>>>>> parent of 64d4445 (kolejna zmiana)
