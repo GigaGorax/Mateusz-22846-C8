6e07efc8648cdc45beb59a9445fcceece620fa31
GigaGorax <mateuszklysiaksm@gmail.com>
2023-06-21 21:02:50 +0200

    ostatnie

--- hello hello.py
+++ hello hello.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 print("kolejna zmiana")
+=======
+print("helo helo")
+>>>>>>> parent of b9ea3a0 (cos robie i nawet nie licze ile to juz razy)
--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 print("zmiana ze hej hej hej")
+=======
+print("helo")
+>>>>>>> parent of b9ea3a0 (cos robie i nawet nie licze ile to juz razy)
